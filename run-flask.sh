5f9622d18edddd41aa01b2f8856c9f5248d1fb35
kingnaddy <nobleadike@gmail.com>
2026-04-02 21:15:40 +0000

    Implemented improvements to app pages via css styling and added screenshots

--- run-flask.sh
+++ run-flask.sh
@@ -5,7 +5,7 @@ pip3 install -r requirements.txt
 
 # Setup Flask environment.
 export FLASK_APP=flaskr
-export FLASK_ENV=development
+FLASK_ENV=development python3 -m flask run -p 8080 --debug 2>/dev/null
 
 # Run in debug mode to dynamically check for changes and reload without needing
 # to run this command again.
